11bd97417287458967ba33feca7965f11e6c1b92
darshank <darshankadam8@gmail.com>
2020-07-02 12:47:08 +0530

    added images

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -7,19 +7,19 @@ const Footer  = () =>{
             <div className="footer__links">
                 <a href="mailto:darshankadam714@gmail.com" target="_blank" rel="noopener noreferrer">
                     <span className="text">email</span>
-                    <img src='email.svg' alt=""/>
+                    <img src={process.env.PUBLIC_URL + '/icons/email.svg'} alt=""/>
                 </a>
                 <a href="https://twitter.com/darshankadam8" target="_blank" rel="noopener noreferrer">
                     <span className="text">Twitter</span>
-                    <img src='twitter.svg' alt="twitter"/>
+                    <img src={process.env.PUBLIC_URL + '/icons/twitter.svg'} alt="twitter"/>
                 </a>
                 <a href="https://www.linkedin.com/in/darshan-kadam-9ba013159/" target="_blank" rel="noopener noreferrer">
                     <span className="text">LinkedIn</span>
-                    <img src='linkedin.svg' alt=""/>
+                    <img  src={process.env.PUBLIC_URL + '/icons/linkedin.svg'}  alt=""/>
                 </a>
                 <a href="https://github.com/DarshanK777" target="_blank" rel="noopener noreferrer">
                     <span className="text">Github</span>
-                    <img src='github.svg' alt=""/>
+                    <img src={process.env.PUBLIC_URL + '/icons/github.svg'} alt=""/>
                 </a>
                 
                  
