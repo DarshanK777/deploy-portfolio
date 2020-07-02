5318dbe1d8bdd5b249c6c8723e4d6704df42fd07
darshank <darshankadam8@gmail.com>
2020-07-02 11:28:18 +0530

    image change

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -7,19 +7,19 @@ const Footer  = () =>{
             <div className="footer__links">
                 <a href="mailto:darshankadam714@gmail.com" target="_blank" rel="noopener noreferrer">
                     <span className="text">email</span>
-                    <img src='/images/email.svg' alt="email"/>
+                    <img src='email.svg' alt=""/>
                 </a>
                 <a href="https://twitter.com/darshankadam8" target="_blank" rel="noopener noreferrer">
                     <span className="text">Twitter</span>
-                    <img src='/images/twitter.svg' alt="email"/>
+                    <img src='twitter.svg' alt="twitter"/>
                 </a>
                 <a href="https://www.linkedin.com/in/darshan-kadam-9ba013159/" target="_blank" rel="noopener noreferrer">
                     <span className="text">LinkedIn</span>
-                    <img src='/images/linkedin.svg' alt="email"/>
+                    <img src='linkedin.svg' alt=""/>
                 </a>
                 <a href="https://github.com/DarshanK777" target="_blank" rel="noopener noreferrer">
                     <span className="text">Github</span>
-                    <img src='/images/github.svg' alt="email"/>
+                    <img src='github.svg' alt=""/>
                 </a>
                 
                  
