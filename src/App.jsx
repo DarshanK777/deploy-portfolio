25b85197c2ab451d3813154f6bdb87ab6695ec0c
dk5761 <darshankadam714@gmail.com>
2023-07-19 20:22:41 +0530

    added redirect

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+
+const App = () => {
+  useEffect(() => {
+    window.location.replace("https://portfolio-dk5761.vercel.app/experience");
+  });
+
+  return <div>test</div>;
+};
+
+export default App;
